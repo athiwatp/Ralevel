51d4b53a54a4f3382610f1cdf154a6bd32df14cd
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2014-11-08 11:04:56 +0700

    litle edit constant

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 define('BASEURL', '');
 define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 define('COREPATH', 'framework/core/');
-define('APPPATH', 'framework/app');
+define('APPPATH', 'framework/app/');
 
-require BASEPATH.'start.php';
+require COREPATH.'start.php';
 
-View::make('layouts.app');
+View::make('layout.app');
 
 echo Url::baseUrl();
