e1ad03a1b08d5dfe842e3a8a280abbbc5bf024f6
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2014-12-29 13:09:31 +0700

    editing some views

--- framework/app/Views/layout/app.php
+++ framework/app/Views/layout/app.php
@@ -1,46 +1,9 @@
 @extends('layout.master')
 
 @section('content')
-<section id="container" class="">
-      <!--header start-->
-      <aside>
-          <div id="sidebar"  class="nav-collapse ">
-              <!-- sidebar menu start-->
-              <ul class="sidebar-menu" id="nav-accordion">
-                  @foreach ($menus as $menu)
-                  <li>
-                      <a {{ (!isset($activeId)) ? '' : (($menu['id'] != $activeId) ? '' : ' class="active"') }} href="#">
-                          <i class="icon-"></i>
-                          <span>{{ strtoupper($menu['name']) }}</span>
-                      </a>
-                  </li>
-                  @endforeach    
-                  <li>
-                    <a href="#modalAddDepartment" data-toggle="modal" class="btn btn-small btn-danger" style="color: white;">Tambah Departemen</a>
-                  </li>            
-              </ul>
-              <!-- sidebar menu end-->
-          </div>
-      </aside>
-      <!--sidebar end-->
-      <!--main content start-->
-      <section id="main-content">
-      @yield('mainContent', 'yep')
-      </section>
-      <!--main content end-->
-      <!--footer start-->
-      <footer class="site-footer">
-          <div class="text-center">
-              2014 &copy; SantriBook by Mokhamad Rofiudin. Special for Al Mubarok Life School.
-              <a href="#" class="go-top">
-                  <i class="icon-angle-up"></i>
-              </a>
-          </div>
-      </footer>
-      <!--footer end-->
-  </section>
-  @stop
+         @yield('mainContent', 'yep')
+@stop
 
-  @section('content2')
+@section('content2')
   <h3>Hello, just  test</h3>
-  @stop
+@stop
--- framework/app/Views/layout/master.php
+++ framework/app/Views/layout/master.php
@@ -6,29 +6,9 @@
     <meta name="description" content="">
     <meta name="author" content="Mosaddek">
     <meta name="keyword" content="FlatLab, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
-    <base href="{{{ asset('static').'/' }}}" >
     <link rel="shortcut icon" href="img/favicon.png">
 
     <title>{{{ $title or '' }}}</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-reset.css" rel="stylesheet">
-    <!--external css-->
-    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-
-    <link rel="stylesheet" type="text/css" href="assets/bootstrap-fileupload/bootstrap-fileupload.css" />
-    <link rel="stylesheet" type="text/css" href="assets/bootstrap-datepicker/css/datepicker.css" />
-    <link rel="stylesheet" type="text/css" href="assets/bootstrap-colorpicker/css/colorpicker.css" />
-    <link rel="stylesheet" type="text/css" href="assets/bootstrap-daterangepicker/daterangepicker.css" />
-    <link href="assets/jcrop/css/jquery.Jcrop.min.css" rel="stylesheet"/>
-    <link href="css/image-crop.css" rel="stylesheet"/>
-
-    <!-- Custom styles for this template -->
-    <link href="css/font-face.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/style-responsive.css" rel="stylesheet" />
-
 
 
 
@@ -47,45 +27,5 @@
     @yield('content2')
 
       <!-- js placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
-    <script src="js/jquery.tmpl.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.scrollTo.min.js"></script>
-    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-
-    <script src="js/jquery-ui-1.9.2.custom.min.js"></script>
-    <script src="js/fileuploader.js"></script>
-    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
-
-  <!--custom switch-->
-  <script src="js/bootstrap-switch.js"></script>
-  <!--custom tagsinput-->
-  <script src="js/jquery.tagsinput.js"></script>
-  <!--custom checkbox & radio-->
-  <script type="text/javascript" src="js/ga.js"></script>
-
-  <script type="text/javascript" src="assets/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-  <script type="text/javascript" src="assets/bootstrap-fileupload/bootstrap-fileupload.js"></script>
-  <script type="text/javascript" src="assets/bootstrap-daterangepicker/date.js"></script>
-  <script type="text/javascript" src="assets/bootstrap-daterangepicker/daterangepicker.js"></script>
-  <script type="text/javascript" src="assets/bootstrap-colorpicker/js/bootstrap-colorpicker.js"></script>
-  <script type="text/javascript" src="assets/jcrop/js/jquery.Jcrop.js"></script>
-  <script type="text/javascript" src="js/bootstrap-typeahead.js"></script>
-  <script type="text/javascript" src="assets/ckeditor/ckeditor.js"></script>
-
-  <script type="text/javascript" src="assets/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
-  <script src="js/respond.min.js" ></script>
-
-
-  <!--common script for all pages-->
-  <script>
-  var $niceScroll;
-  </script>
-    <script src="js/common-scripts.js"></script>
-
-  <!--script for this page-->
-  <script src="js/form-component.js"></script>
-
-  <script src="js/main.js"></script>
   </body>
 </html>
